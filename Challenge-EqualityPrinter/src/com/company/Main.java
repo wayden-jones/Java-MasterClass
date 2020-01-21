285da0391bb4b48495bc57e427ccd9c29f7113d8
wayden-developer <wayden.jones1@gmail.com>
2020-01-21 10:24:25 -0400

    Equality Challenge

--- Challenge-EqualityPrinter/src/com/company/Main.java
+++ Challenge-EqualityPrinter/src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        printEqual(1, 1, 1);
+        printEqual(1, 1, 2);
+        printEqual(-1, -1, -1);
+        printEqual(1, 2, 3);
+    }
+
+    public static void printEqual(int x, int y, int z){
+        if(x < 0 || y < 0 || z < 0){
+            System.out.println("Invalid Value");
+            return;
+        }
+        if(x == y && y == z){
+            System.out.println("All numbers are equal");
+            return;
+        }
+        if(x != y && z != x && y != z){
+            System.out.println("All numbers are different");
+            return;
+        }
+        System.out.println("Neither all are equal or different");
     }
 }
