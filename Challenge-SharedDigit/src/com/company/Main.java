c97f411ec9a55779a30d5f2852de3d8a8ecf5de3
wayden-developer <wayden.jones1@gmail.com>
2020-01-24 10:45:42 -0400

    Challenges Galore

--- Challenge-SharedDigit/src/com/company/Main.java
+++ Challenge-SharedDigit/src/com/company/Main.java
@@ -3,12 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(hasSharedDigit(12, 32));
     }
     public static boolean hasSharedDigit(int x, int y)
     {
         if(x > 99 || x < 10 || y > 99 || y < 10)
             return false;
-        
+        int firstX, secondX, firstY, secondY = 0;
+        secondX = x%10;
+        secondY = y%10;
+        firstX = x/10;
+        firstY = y/10;
+        if(firstX == firstY || firstX == secondY || secondX == firstY || secondX == secondY)
+            return true;
+        else
+            return false;
     }
 }
